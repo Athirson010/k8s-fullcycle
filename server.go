c5331e6b0e2e2dbb2cbfd10975e50ee48ee18aba
athirson.candido <athirson.candido@bandtec.com.br>
2023-11-07 14:38:56 -0300

    add: projeto k8s

---
+++ server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var startedAt = time.Now()
+
+func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":80", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	nome := os.Getenv("NOME")
+	idade := os.Getenv("IDADE")
+
+	w.Write([]byte("<h1>Fullcycle app go!!</h1>"))
+	w.Write([]byte("<h1>Loadbalancer com configmap</h1>"))
+	fmt.Fprintf(w, "Salve, eu sou o %s. e eu tenho %s anos de idade.", nome, idade)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("minhaFamilia/familia.txt")
+	if err != nil {
+		log.Fatalf("Erro ao ler o arquivo", string(data))
+	}
+	fmt.Fprintf(w, "Minha familia: %s. ", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password %s.", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
+}
